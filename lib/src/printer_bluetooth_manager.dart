64a6edc3b2b56223c220904cff1a5d72fb7d8f22
Yczar <babalolagbogo@gmail.com>
2022-05-29 01:16:27 +0100

    updated files

--- lib/src/printer_bluetooth_manager.dart
+++ lib/src/printer_bluetooth_manager.dart
@@ -8,7 +8,7 @@
 
 import 'dart:async';
 import 'dart:io';
-import 'package:esc_pos_utils/esc_pos_utils.dart';
+import 'package:flutter_scan_bluetooth/flutter_scan_bluetooth.dart';
 import 'package:rxdart/rxdart.dart';
 import 'package:flutter_bluetooth_basic/flutter_bluetooth_basic.dart';
 import './enums.dart';
